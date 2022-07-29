94d27a0bec54416749f366f48339d7596d26ff09
Philipp <github@piwo.space>
2022-07-29 18:45:55 +0200

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  setupFiles: ["<rootDir>/tests/unit/setup-tests.js"],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
     '\\.(aes)$': '<rootDir>/jest-ignore-aes.js'
+  },
+  globals: {
+    Uint8Array: Uint8Array,
   }
 }
